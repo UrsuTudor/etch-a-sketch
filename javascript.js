53cd0a8df897b6b18e4c70e9abb391f7c4c824e3
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-21 18:14:03 +0200

    Fix bottom gap between rows

--- javascript.js
+++ javascript.js
@@ -2,5 +2,6 @@ const container = document.querySelector('.container')
 for (i = 1; i <= 256; i++) {
     const div = document.createElement('div')
     div.classList.add('squares')
+    div.textContent = i
     container.appendChild(div)
 }
