2c8d172373aedc27b8422a13fadfe3aed78e0d16
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-22 16:27:15 +0200

    Make button remove previous grid and add squares equal to prompt
    Still need to figure out how to make a grid that has the same amount
    of squares on each side and fills the available space.

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ for (i = 1; i <= 256; i++) {
     container.appendChild(div);
 }
 
-const squares = document.querySelectorAll('.squares')
+let squares = document.querySelectorAll('.squares')
 
 squares.forEach((square) => {
     square.addEventListener('mouseenter', ()  => {
@@ -19,4 +19,13 @@ container.appendChild(newGridBtn)
 
 newGridBtn.addEventListener('click', () => {
     let numberOfSquares = prompt('How many pixels would you like your canvas to have on each side?')
+    squares.forEach((square) => {
+        container.removeChild(square)
+    });
+    for (i = 1; i <= numberOfSquares; i++) {
+        const div = document.createElement('div');
+        div.classList.add('squares');
+        container.appendChild(div);
+    };
+    squares = document.querySelectorAll('.squares')
 })
