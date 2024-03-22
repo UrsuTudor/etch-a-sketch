be691f840842469aeb437e74c0b1fb7d4eaecfd0
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-22 16:23:16 +0200

    Revert "Made to delete"
    This reverts commit ba1da6673c76902282798bf3ebc6820a63f5f615.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 const container = document.querySelector('.container')
 for (i = 1; i <= 256; i++) {
     const div = document.createElement('div');
-    div.textContent = i
     div.classList.add('squares');
     container.appendChild(div);
 }
@@ -14,24 +13,10 @@ squares.forEach((square) => {
     })
 })
 
-const btnContainer = document.querySelector('.btnContainer')
 let newGridBtn = document.createElement('button')
 newGridBtn.textContent = 'Reset canvas'
-btnContainer.appendChild(newGridBtn)
+container.appendChild(newGridBtn)
 
-function createNewCanvas(numOfSquares) {
-    numberOfSquares = prompt('How many pixels would you like your canvas to have on each side?');
-    for (i = 1; i <= (numberOfSquares * numberOfSquares); i++) {
-        const div = document.createElement('div');
-        div.classList.add('squares');
-        div.textContent = i
-        container.appendChild(div);
-    }
-}
 newGridBtn.addEventListener('click', () => {
-    squares.forEach((square) => {
-        container.removeChild(square)
-    });
-    createNewCanvas();
+    let numberOfSquares = prompt('How many pixels would you like your canvas to have on each side?')
 })
-
